3f1de9fb6d3a8a92da7acea54e6e5b395783e1e0
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2020-10-02 11:39:13 -0500

    Prueba01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string dataType;
+
+    getline(cin >> ws, dataType);
+
+    if(dataType == "int")
+        cout << sizeof(int) << endl;
+    
+    if(dataType == "char")
+        cout << sizeof(char) << endl;
+    
+    if(dataType == "short")
+        cout << sizeof(short) << endl;
+    
+    if(dataType == "long")
+        cout << sizeof(long) << endl;
+
+    if(dataType == "float")
+        cout << sizeof(float) << endl;
+
+    if(dataType == "double")
+        cout << sizeof(double) << endl;
+    
 
     return 0;
 }
